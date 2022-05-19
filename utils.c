99abbf7940e71aff23bf78021adfe04d65a8d63b
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-05-19 14:36:08 -0700

    commit

---
+++ utils.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+ * _stlen - calculate the length of a string
+ * @str: string
+ *
+ * Return: Length
+ * */
+int _strlen(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		;
+
+	return (i);
+}
+
+/**
+ * print - print char.
+ * @str: pstring.
+ *
+ * Return: string length.
+ */
+
+int print(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		_putchar(str[i]);
+
+	return (i);
+}
+
